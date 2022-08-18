795b70262c867777582e50834bff1161caa6d2d3
Line-em <alineemily@hotmail.com>
2022-08-18 21:23:10 +0100

    useNavigate on Book.jsx

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import booksData from "../assets/booksData";
 
 export default function Book(props) {
@@ -10,16 +10,34 @@ export default function Book(props) {
 		return book.id.toString() === params.id;
 	});
 
-	const maxDescriptionLength = 200;
-
-	const [isShortDescription, setIsShortDescription] = useState(true);
-
-	const toggleDescription = () => setIsShortDescription(!isShortDescription);
+	// At the course, it is used useHistory. This doesn't exist in v6.
+	// In this case, we can simply use useNavigate.
+	const navigate = useNavigate();
+	const goBack = () => {
+		navigate("/booklist");
+	};
+	// To go forward or backwards, you could also use:
+	// const goForward = () => {
+	// 	navigate(`/booklist/${currentBook.id + 1}`);
+	// };
+	// const goBack = () => {
+	// 	navigate(`/booklist/${currentBook.id - 1}`);
+	// };
+	// Or, also, based on the history:
+	// const goForward = () => {
+	// 	navigate(1);
+	// };
+	// const goBack = () => {
+	// 	navigate(-1);
+	// };
 
 	// toggle between read more and read less, using substring to shorten the description.
+	const [isShortDescription, setIsShortDescription] = useState(true);
+	const maxDescriptionLength = 200;
 	const shortDescription = currentBook.description.substring(0, maxDescriptionLength);
 	const longDescription = currentBook.description;
 	const description = isShortDescription ? shortDescription : longDescription;
+	const toggleDescription = () => setIsShortDescription(!isShortDescription);
 
 	return (
 		<div>
@@ -42,6 +60,8 @@ export default function Book(props) {
 					) : (
 						<button onClick={toggleDescription}>Read Less</button>
 					)}
+					<br />
+					<button onClick={goBack}>Go Back to Booklist</button>
 				</p>
 				<img src={currentBook.image} alt={currentBook.title} />
 			</div>
